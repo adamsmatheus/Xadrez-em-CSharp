99d368cac9aa245b271291e62c84709f810671a9
Matheus Adams <adamsmatheus7@gmail.com>
2020-04-06 20:48:07 -0300

    Adicionando a classe conversao de posicao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Tabuleiro;
 using Xadrez.Tabuleiro;
+using Xadrez.Xadrez;
 
 namespace Xadrez
 {
@@ -8,20 +9,8 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Taboleiro tab = new Taboleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-                Tela.imprimirTabuleiro(tab);
-                Console.ReadLine();
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos.ToPosicao());
         }
     }
 }
