0b62644e8ed9e7046043cd10fa563d87eaca8fcb
Matheus Adams <adamsmatheus7@gmail.com>
2020-04-06 16:43:13 -0300

    Criada a classe Posicao

--- Xadrez/Tabuleiro/Posicao.cs
+++ Xadrez/Tabuleiro/Posicao.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-namespace Xadrez.Tabuleiro
+namespace Tabuleiro
 {
     public class Posicao
     {
+
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
         public Posicao()
         {
+        }
+
+        public override string ToString()
+        {
+            return Linha + "," + Coluna; 
         }
     }
 }
