f176638b3509122e5415652ce6553c098b5cc8e2
Matheus Adams <adamsmatheus7@gmail.com>
2020-04-07 01:07:20 -0300

    Adicionando cor

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -9,8 +9,20 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos.ToPosicao());
+            try
+            {
+                Taboleiro tab = new Taboleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+
+            }catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message); 
+
+            }
+            Console.ReadLine();
         }
     }
 }
--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -15,6 +15,7 @@ namespace Xadrez
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i +" ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -27,6 +28,26 @@ namespace Xadrez
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+
+            }
+            else
+            {
+
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+
+
         }
     }
 }
